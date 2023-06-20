119b27c585ec48ed420928c92ebb14bb5c15cbdd
nohtaejin <ntj362@gmail.com>
2023-06-20 17:33:47 +0900

    second-commit

--- firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
+++ firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
@@ -2,9 +2,19 @@ package com.example.firstservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/first-service/")
 public class FirstserviceApplication {
+
+	@GetMapping(value="/welcome")
+	public String welcome(){
+		return "Welcome to the First service";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstserviceApplication.class, args);
--- secondservice/src/main/java/com/example/secondservice/SecondserviceApplication.java
+++ secondservice/src/main/java/com/example/secondservice/SecondserviceApplication.java
@@ -2,10 +2,18 @@ package com.example.secondservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/second-service/")
 public class SecondserviceApplication {
-
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to the second service";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SecondserviceApplication.class, args);
 	}
